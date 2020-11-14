eef8c756a3f698db7c5c70f4f6fa7976ac2d7b35
Shafik Amin <shafik23@gmail.com>
2020-11-14 12:27:03 -0700

    Added post route to biscotty

--- biscotty/app/Main.hs
+++ biscotty/app/Main.hs
@@ -1,4 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Web.Scotty (ScottyM, get, html, param, post, scotty)
+
 main :: IO ()
-main = print "shafik"
+main = scotty 3000 $ displayWord >> postWord
+
+displayWord :: ScottyM ()
+displayWord = get "/:word" $ do
+  beam <- param "word"
+  html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
+
+postWord :: ScottyM ()
+postWord = post "/test" $ do
+  html "POSTED"
